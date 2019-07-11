591047e10dd25c0bfa93f9c98ad6607cd6a73b0b
slava <slava15.92@gmail.com>
2019-07-11 19:11:08 +0300

    Update the logic connection between the server and the client

--- carRace/client/game.js
+++ carRace/client/game.js
@@ -1,47 +1,64 @@
 const connection = new WebSocket('ws://localhost:9090');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-context.canvas.width  = window.innerWidth;
-context.canvas.height  = window.innerHeight;
+context.canvas.width = window.innerWidth;
+context.canvas.height = window.innerHeight;
 
 '<?php$session = mt_rand(1,999);?>'
-let car1 = null;
-let car2 = null;
-let cone;
-let background;
-let money;
-let road = null;
-let current_player_arrow = null;
-let ready;
+let car1 = new Image();
+let car2 = new Image();
+let cone = new Image();
+let background = new Image();
+let money = new Image();
+let road = new Image();
+let current_player_arrow = new Image();
 let speedSound;
 let basePos = 0;
 let carPlace = 0;
 let myMoney = 0;
 let otherMoney = 0;
 let matrix = [];
+let baseCar1;
+let baseCar2;
+let car1y;
+let car2y;
 
-for(let i=0; i<6; i++) {
+for (let i = 0; i < 6; i++) {
     matrix[i] = new Array(3);
 }
 
-loadGame();
-
-let baseCar1 = canvas.height * 0.1 + road.height;
-let baseCar2 = canvas.height * 0.6 + road.height;
-let car1y = baseCar1;
-let car2y = baseCar2;
-
 function loadGame() {
     canvas.style.border = "1px solid black";
+    background.onload = function () {
+        context.drawImage(background, basePos, basePos, background.width, background.height, basePos, basePos, canvas.width, canvas.height);
+    };
 
-    background = new Image();
-    road = new Image();
-    car1 = new Image();
-    car2 = new Image();
-    current_player_arrow = new Image();
-    ready = new Image();
-    money = new Image();
-    cone = new Image();
+
+    road.onload = function () {
+        context.drawImage(road, basePos, canvas.height * 0.1, canvas.width, road.height * 3);
+        context.drawImage(road, basePos, canvas.height * 0.6, canvas.width, road.height * 3);
+        baseCar1 = canvas.height * 0.1 + road.height;
+        baseCar2 = canvas.height * 0.6 + road.height;
+        car1y = baseCar1;
+        car2y = baseCar2;
+    };
+    
+    car1.onload = function () {
+        context.drawImage(car1, basePos, car1y);
+    };
+    car2.onload = function () {
+        context.drawImage(car2, basePos, car2y);
+    };
+    current_player_arrow.onload = function () {
+        context.drawImage(current_player_arrow, basePos + car1.width / 2, car1y - 30);
+    };
+
+    money.onload = function () {
+        console.log("hi\n");
+    };
+    cone.onload = function () {
+        console.log("hi\n");
+    };
     speedSound = new Audio();
 
     background.src = "images/background.png";
@@ -49,7 +66,6 @@ function loadGame() {
     car1.src = "images/car1.png";
     car2.src = "images/car2.png";
     current_player_arrow.src = "images/current_player_arrow.png";
-    ready.src = "images/ready.png";
     money.src = "images/money.png";
     cone.src = "images/cone.png";
     speedSound.src = "sounds/car_speed.wav";
@@ -65,39 +81,37 @@ function drawCanvas() {
 
     for (let i = 1; i < 6; i++) {
         for (let j = 0; j < 3; j++) {
-            if(typeof  matrix[i][j] !== "undefined"){
-                if(matrix[i][j]==='money')
-                {
-                    context.drawImage(money, basePos + i *170,  baseCar1- 20 *j,money.width*0.4,money.height*0.4);
-                    context.drawImage(money, basePos + i *170,  baseCar2- 20 *j,money.width*0.4,money.height*0.4);
-                }else{
-                    context.drawImage(cone, basePos+i *170,  baseCar1 - 20 *j,cone.width*0.1,cone.height*0.1);
-                    context.drawImage(cone, basePos+i *170,  baseCar2 - 20 *j,cone.width*0.1,cone.height*0.1);
+            if (typeof matrix[i][j] !== "undefined") {
+                if (matrix[i][j] === 'money') {
+                    context.drawImage(money, basePos + i * 170, baseCar1 - 20 * j, money.width * 0.4, money.height * 0.4);
+                    context.drawImage(money, basePos + i * 170, baseCar2 - 20 * j, money.width * 0.4, money.height * 0.4);
+                } else {
+                    context.drawImage(cone, basePos + i * 170, baseCar1 - 20 * j, cone.width * 0.1, cone.height * 0.1);
+                    context.drawImage(cone, basePos + i * 170, baseCar2 - 20 * j, cone.width * 0.1, cone.height * 0.1);
                 }
             }
         }
     }
 
-    if(typeof  matrix[0][carPlace+1] !== "undefined") {
-        if(matrix[0][carPlace+1]==='money')
-        {
+    if (typeof matrix[0][carPlace + 1] !== "undefined") {
+        if (matrix[0][carPlace + 1] === 'money') {
             myMoney++;
             connection.send(
                 JSON.stringify({
                     'type': 'IGetMoney'
                 })
             );
-        }else{
+        } else {
             connection.send(
                 JSON.stringify({
                     'type': 'ILostYouWon'
                 })
             );
-            alert("You lost! Opponent won with "+myMoney+" points");
+            alert("You lost! Opponent won with " + myMoney + " points");
         }
     }
 }
-
+loadGame();
 drawCanvas();
 
 connection.onopen = function () {
@@ -127,16 +141,16 @@ connection.onmessage = function (e) {
             break;
 
         case "cone":
-            case "money":
-                for(let i=0; i<5; i++) {
-                    matrix[i] = matrix[i+1];
-                }
-                matrix[5]=new Array(3);
-                matrix[5][json.floor]=json.type;
-                drawCanvas();
-                break;
+        case "money":
+            for (let i = 0; i < 5; i++) {
+                matrix[i] = matrix[i + 1];
+            }
+            matrix[5] = new Array(3);
+            matrix[5][json.floor] = json.type;
+            drawCanvas();
+            break;
         case 'ILostYouWon':
-            alert("Congrats! You won!"+myMoney);
+            alert("Congrats! You won!" + myMoney);
             break;
     }
 };
@@ -170,5 +184,3 @@ document.onkeypress = (event) => {
             break;
     }
 };
-
-context.drawImage(current_player_arrow, basePos +car1.width/2, car1y - 30 );
