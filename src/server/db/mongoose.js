ede5f5c25c8e41c85aa2d3505bcb48b1f5791924
YossiSaadi <yossisaadi@gmail.com>
2019-07-20 12:57:25 +0300

    final mongoose connection file

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,5 +1,6 @@
 const { connect } = require('mongoose');
 
+// ON CMD NOT BASH ==> "/Users/YOSSI SAADI\mongodb/bin/mongod.exe" --dbpath="/Users/YOSSI SAADI/mongodb-data"
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'facefeka-db';
 
