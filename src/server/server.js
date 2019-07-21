dc579d850e6a2df0cf4105c6629270200e0b56f0
YossiSaadi <yossisaadi@gmail.com>
2019-07-22 02:02:28 +0300

    added the use postRouter and get method for any route that is not in user/post route

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require(`express`);
 require(`./db/mongoose`);
+
 const userRouter = require(`./routers/user_router`);
+const postRouter = require(`./routers/post_router`);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 const client = (path.join(__dirname, '../../'));
 app.use(express.static(client));
@@ -12,7 +14,13 @@ app.use(express.static(client));
 app.use(express.json());
 
 app.use(userRouter);
-// app.use(someOtherRouter);
+app.use(postRouter);
+
+app.get('*', (req, res) => {
+  res.status(404).send({
+    error: `404!#@#!!! Sorry, Unfortunately Facefeka doesn't have any page linked to address '${req.params[0]}'`
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
